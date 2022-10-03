3ef48ac20c581a37fca14a518b2579805525e1b3
Marc Busqué <marc@lamarciana.com>
2022-10-03 07:00:14 +0200

    bundle gem kwork
    With options:
    - --no-exe
    - --coc
    - --mit
    - --test=rspec
    - --ci=github
    - --linter=rubocop
    
    Answering yes to generating a CHANGELOG file.
    
    [skip ci]

---
+++ spec/kwork_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kwork do
+  it "has a version number" do
+    expect(Kwork::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
